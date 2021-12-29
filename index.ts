bf5555c4bf3d4ef640a27baa3bd165b10d1e0eea
Seth Battis <seth@battis.net>
2021-12-29 10:00:20 -0800

    Publish to npm

---
+++ index.ts
@@ -0,0 +1,15 @@
+export type AsynchronousFunction = () => Promise<any>;
+export type Nullable<T> = T | null;
+export type Optional<T> = T | undefined;
+export type Subset<T, U extends T> = T;
+export type Constructor<T> = new (...args: any[]) => T;
+export type Mixin<T extends (...args: any[]) => any> = InstanceType<
+    ReturnType<T>
+>;
+// https://stackoverflow.com/a/65152869/294171
+export function instanceOf<TElements, TFilter extends TElements>(
+    array: TElements[],
+    filterType: Constructor<TFilter>
+): TFilter[] {
+    return <TFilter[]>array.filter((e) => e instanceof filterType);
+}
