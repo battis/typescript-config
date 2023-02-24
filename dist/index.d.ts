9ec353107f9c47ff634a1c58dd02e76e3cfd1214
Seth Battis <sbattis@groton.org>
2023-02-24 12:45:12 -0800

    cleaning

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type AsynchronousFunction = () => Promise<any>;
-export declare type Nullable<T> = T | null;
-export declare type Optional<T> = T | undefined;
-export declare type Subset<T, U extends T> = T;
-export declare type Constructor<T> = new (...args: any[]) => T;
-export declare type Mixin<T extends (...args: any[]) => any> = InstanceType<ReturnType<T>>;
-export declare function instanceOf<TElements, TFilter extends TElements>(array: TElements[], filterType: Constructor<TFilter>): TFilter[];
-export declare const isConstructor: (value: any) => value is Constructor<any>;
