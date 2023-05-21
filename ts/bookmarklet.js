6289ca6ae1c0b645212572f968f5428bc1501399
Seth Battis <seth@battis.net>
2023-05-21 09:56:13 -0400

    trying to generate README

--- ts/bookmarklet.js
+++ ts/bookmarklet.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CreateFilePlugin = require('create-file-webpack');
 
 module.exports = ({ root, package, title, externals = {} }) => {
   const HtmlWebpackPage = (page) =>
@@ -53,7 +54,17 @@ module.exports = ({ root, package, title, externals = {} }) => {
       new CleanWebpackPlugin(),
       new Dotenv(),
       HtmlWebpackPage('install.html'),
-      HtmlWebpackPage('embed.html')
+      HtmlWebpackPage('embed.html'),
+      new CreateFilePlugin({
+        filename: 'README.md',
+        content: `# ${package.name}
+
+${package.description}
+
+## Install
+
+${require(path.resolve(root, 'build/embed.html'))}`
+      })
     ],
     optimization: {
       minimize: true,
