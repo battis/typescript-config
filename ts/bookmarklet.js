374f0833c91c013a6bd938c53b6855da109faa2f
Seth Battis <seth@battis.net>
2023-05-21 09:42:25 -0400

    bookmarklet draft 2

--- ts/bookmarklet.js
+++ ts/bookmarklet.js
@@ -1,49 +1,38 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-const HtmlWebpackPage = ({ page, package, template, bookmarkletTitle }) =>
-  new HtmlWebpackPlugin({
-    templateParameters: {
-      REPO_OWNER: package.repository.url.replace(
-        /\/github.com\/([^/]+)\/.*/,
-        '$1'
+module.exports = ({ root, package, title, externals = {} }) => {
+  const HtmlWebpackPage = (page) =>
+    new HtmlWebpackPlugin({
+      templateParameters: {
+        REPO_OWNER: package.repository.url.replace(
+          /\/github.com\/([^/]+)\/.*/,
+          '$1'
+        ),
+        REPO_NAME: package.repository.url.replace(
+          /\/github.com\/[^/]+\/([^/]+)\/.*/,
+          '$1'
+        ),
+        TITLE: title
+      },
+      template: path.resolve(
+        root,
+        'node_modules/@battis/webpack/template/bookmarklet',
+        page
       ),
-      REPO_NAME: package.repository.url.replace(
-        /\/github.com\/[^/]+\/([^/]+)\/.*/,
-        '$1'
-      ),
-      BOOKMARKLET_TITLE: bookmarkletTitle
-    },
-    template: path.join(template, page),
-    hash: true
-  });
+      filename: page,
+      inject: false,
+      hash: true
+    });
 
-module.exports = ({
-  root,
-  bookmarkletTitle,
-  package,
-  entry = './src/index.ts',
-  build = 'build',
-  externals = {},
-  terserOptions = undefined
-}) => {
-  const template = path.resolve(
-    root,
-    'node_modules',
-    '@battis/webpack',
-    'template',
-    'bookmarklet'
-  );
   return {
     mode: 'production',
-    entry: { main: entry },
+    entry: { main: './src/index.ts' },
     output: {
-      path: path.resolve(root, build),
+      path: path.resolve(root, 'build'),
       filename: 'bookmarklet.js'
     },
     module: {
@@ -53,73 +42,22 @@ module.exports = ({
           use: {
             loader: 'ts-loader'
           }
-        },
-        {
-          test: /\.svg$/,
-          use: 'raw-loader'
-        },
-        {
-          test: /\.s?[ac]ss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: { importLoaders: 2 }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: ['postcss-preset-env']
-                }
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: { implementation: require('sass') }
-            }
-          ]
-        },
-        {
-          test: /\.(jpe?g|gif|png)/,
-          loader: 'file-loader',
-          options: {
-            name: '[name].[contenthash].[ext]',
-            outputPath: 'assets/images'
-          }
         }
       ]
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
     },
-    externals: externals,
+    externals,
     plugins: [
       new CleanWebpackPlugin(),
       new Dotenv(),
-      HtmlWebpackPage({
-        page: 'index.html',
-        package,
-        bookmarkletTitle,
-        template
-      }),
-      HtmlWebpackPage({
-        page: 'embed.html',
-        package,
-        bookmarkletTitle,
-        template
-      }),
-      new MiniCssExtractPlugin({
-        filename: 'stylesheet.css'
-      })
+      HtmlWebpackPage('install.html'),
+      HtmlWebpackPage('embed.html')
     ],
     optimization: {
       minimize: true,
-      minimizer: [
-        '...',
-        new TerserPlugin(terserOptions),
-        new CssMinimizerWebpackPlugin()
-      ],
+      minimizer: ['...', new TerserPlugin()],
       splitChunks: { chunks: 'all' }
     }
   };
