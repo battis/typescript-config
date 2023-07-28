799a4b290cddefebe442751b6af93ef52ef549f2
Seth Battis <seth@battis.net>
2023-07-27 20:45:49 -0400

    AssociativeArray

--- src/Shorthand.ts
+++ src/Shorthand.ts
@@ -2,3 +2,4 @@ export type AsynchronousFunction = () => Promise<any>;
 export type Nullable<T> = T | null;
 export type Optional<T> = T | undefined;
 export type Subset<T, U extends T> = T;
+export type AssociativeArray<T> = { [key: string]: T };
