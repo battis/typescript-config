4f1a267cf6b14bec4dc2efc09187b234e4a8d56a
Seth Battis <seth@battis.net>
2023-07-31 16:55:41 -0400

    RecusivePartial

--- src/Constructor.ts
+++ src/Constructor.ts
@@ -1,13 +1,18 @@
 export type Constructor<T> = new (...args: any[]) => T;
 
 export const isConstructor = (value: any): value is Constructor<any> => {
-    return !!value && !!value.prototype && !!value.prototype.constructor;
+  return !!value && !!value.prototype && !!value.prototype.constructor;
 };
 
-// https://stackoverflow.com/a/65152869/294171
+/**
+ * TODO is this even useful?
+ *   Not sure why I included it, it seems more likely that it's related to
+ *   wanting [this](https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards)?
+ * @ see https://stackoverflow.com/a/65152869/294171
+ */
 export function instanceOf<TElements, TFilter extends TElements>(
-    array: TElements[],
-    filterType: Constructor<TFilter>
+  array: TElements[],
+  filterType: Constructor<TFilter>
 ): TFilter[] {
-    return <TFilter[]>array.filter((e) => e instanceof filterType);
+  return <TFilter[]>array.filter((e) => e instanceof filterType);
 }
--- src/PropertyRequirements.ts
+++ src/PropertyRequirements.ts
@@ -1,17 +1,26 @@
 // https://stackoverflow.com/a/49725198/294171
 export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
-    T,
-    Exclude<keyof T, Keys>
+  T,
+  Exclude<keyof T, Keys>
 > &
-    {
-        [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
-    }[Keys];
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
 
 export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
-    T,
-    Exclude<keyof T, Keys>
+  T,
+  Exclude<keyof T, Keys>
 > &
-    {
-        [K in Keys]-?: Required<Pick<T, K>> &
-        Partial<Record<Exclude<Keys, K>, undefined>>;
-    }[Keys];
+  {
+    [K in Keys]-?: Required<Pick<T, K>> &
+    Partial<Record<Exclude<Keys, K>, undefined>>;
+  }[Keys];
+
+/** @see https://stackoverflow.com/a/51365037 */
+export type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+  ? RecursivePartial<U>[]
+  : T[P] extends object | undefined
+  ? RecursivePartial<T[P]>
+  : T[P];
+};
