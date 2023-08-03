a9d90ec9f5b5f03af26548de2fee9e09a034a93d
Seth Battis <seth@battis.net>
2023-08-03 07:55:41 -0400

    editorial comments

--- src/Shorthand.ts
+++ src/Shorthand.ts
@@ -1,6 +1,14 @@
 export type AsynchronousFunction = () => Promise<any>;
+
+/** @deprecated `null` is part of type unless using utility type `Nonnullable` */
 export type Nullable<T> = T | null;
+
+/** @deprecated use `?` */
 export type Optional<T> = T | undefined;
+
 export type Subset<T, U extends T> = T;
+
+/** @deprecated use `Record<string,T>` */
 export type AssociativeArray<T> = { [key: string]: T };
+
 export type JSONPrimitiveTypes = string | number | boolean | null;
