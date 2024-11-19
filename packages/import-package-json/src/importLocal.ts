df3123c5b21aa062c1f064892c0ccb2a3e9e0b3f
Seth Battis <seth@battis.net>
2024-11-19 13:31:42 -0500

    oy, javascript
    send both assert and with type: json when importing

--- packages/import-package-json/src/importLocal.ts
+++ packages/import-package-json/src/importLocal.ts
@@ -1,12 +1,14 @@
-import path from 'path'
 import { IPackageJson } from 'package-json-type';
+import path from 'path';
 
 export default async function importLocal(packageName: string) {
-  return (await import(
+  return (
+    await import(
       path.join(
-          packageName,
-          path.basename(packageName) != 'package.json' ? 'package.json' :''
+        packageName,
+        path.basename(packageName) != 'package.json' ? 'package.json' : ''
       ),
-      { assert: { type: 'json' }}
-  )).default as IPackageJson;
+      { assert: { type: 'json' }, with: { type: 'json' } }
+    )
+  ).default as IPackageJson;
 }
