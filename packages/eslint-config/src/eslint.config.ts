727f075c4832145fc723973a868c87179a72fc32
Seth Battis <seth@battis.net>
2026-01-25 11:39:31 -0500

    feat: unused-vars named ^_.* do not trigger warnings

--- packages/eslint-config/src/eslint.config.ts
+++ packages/eslint-config/src/eslint.config.ts
@@ -11,6 +11,20 @@ export default defineConfig(
   tseslint.configs.recommended,
   eslintConfigPrettier,
   {
+    files: ['**/*.ts', '**/*.mts', '**/*.cts', '**/*.tsx'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  {
     files: ['**/*.html'],
     plugins: { html }
   },
