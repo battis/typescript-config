e97da7ac3bac43f109ff989e252665a7f5c34868
Seth Battis <seth@battis.net>
2026-03-07 11:52:47 -0500

    feat: identify an array of entries with isEntries()

--- packages/typescript-tricks/src/Array.ts
+++ packages/typescript-tricks/src/Array.ts
@@ -1,3 +1,26 @@
 // https://stackoverflow.com/a/51399781/294171
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export function isEntries<
+  K extends string | number | symbol = string | number | symbol,
+  V = unknown
+>(
+  obj: unknown,
+  isK?: (k: unknown) => k is K,
+  isV?: (v: unknown) => v is V
+): obj is [K, V][] {
+  return (
+    Array.isArray(obj) &&
+    (obj.length === 0 ||
+      obj.reduce(
+        (entries, elt) =>
+          entries &&
+          Array.isArray(elt) &&
+          elt.length == 2 &&
+          (!isK || isK(elt[0])) &&
+          (!isV || isV(elt[1])),
+        true
+      ))
+  );
+}
