3f6afc66cf46ce79282dd59d83184c6b70a256ee
Seth Battis <seth@battis.net>
2026-03-07 11:50:08 -0500

    refactor: collect into single Properties module

--- packages/typescript-tricks/src/OptionalProperty.ts
+++
@@ -1,3 +0,0 @@
-//https://stackoverflow.com/a/54178819
-export type OptionalProperty<T, K extends keyof T> = Omit<T, K> &
-  Partial<Pick<T, K>>;
--- packages/typescript-tricks/src/Properties.ts
+++ packages/typescript-tricks/src/Properties.ts
@@ -1,6 +1,4 @@
-/**
- * @see {@link https://stackoverflow.com/a/49725198/294171 StackOverflow response} on requiring at least one property
- */
+/** @see {@link https://stackoverflow.com/a/49725198/294171 StackOverflow response} on requiring at least one property */
 export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
   T,
   Exclude<keyof T, Keys>
@@ -9,34 +7,32 @@ export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
     [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
   }[Keys];
 
-/**
- * @see {@link https://stackoverflow.com/a/49725198/294171 StackOverflow response} on requiring only one property
- */
+/** @see {@link https://stackoverflow.com/a/49725198/294171 StackOverflow response} on requiring only one property */
 export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
   T,
   Exclude<keyof T, Keys>
 > &
   {
     [K in Keys]-?: Required<Pick<T, K>> &
-    Partial<Record<Exclude<Keys, K>, undefined>>;
+      Partial<Record<Exclude<Keys, K>, undefined>>;
   }[Keys];
 
 /** @see {@link https://stackoverflow.com/a/51365037 StackOverflow response} */
 export type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
-  ? RecursivePartial<U>[]
-  : T[P] extends object | undefined
-  ? RecursivePartial<T[P]>
-  : T[P];
+    ? RecursivePartial<U>[]
+    : T[P] extends object | undefined
+      ? RecursivePartial<T[P]>
+      : T[P];
 };
 
 /**
  * ```ts
  * type A = {
- *   d: number,
- *   e: string
- *   f: boolean
- * }
+ *   d: number;
+ *   e: string;
+ *   f: boolean;
+ * };
  *
  * type B = ReplaceProperty<A, 'e', number[]>;
  * // type B = {
@@ -45,6 +41,7 @@ export type RecursivePartial<T> = {
  * //   f: boolean
  * // }
  * ```
+ *
  * @see {@link https://stackoverflow.com/a/51599774 StackOverflow response}
  */
 export type ReplaceProperty<T, K extends keyof T, TReplace> = Identity<
@@ -53,3 +50,7 @@ export type ReplaceProperty<T, K extends keyof T, TReplace> = Identity<
   }
 >;
 type Identity<T> = { [P in keyof T]: T[P] };
+
+//https://stackoverflow.com/a/54178819
+export type OptionalProperty<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
