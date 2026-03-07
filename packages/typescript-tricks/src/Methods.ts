dea03d65ca12555687351909e8e5fa27614270dd
Seth Battis <seth@battis.net>
2026-03-07 11:48:41 -0500

    refactor: rename more generally as Methods

--- packages/typescript-tricks/src/Methods.ts
+++ packages/typescript-tricks/src/Methods.ts
@@ -1,14 +1,14 @@
-/**
- * @see https://stackoverflow.com/a/61940388
- */
-export type MethodNames<T extends { [K in keyof T]: any }> = {
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+
+/** @see https://stackoverflow.com/a/61940388 */
+export type MethodNames<T extends { [K in keyof T]: unknown }> = {
   [K in keyof T]: T[K] extends Function // is this key a function?
     ? K // yes, return the key
     : never; // no, return never
 }[keyof T]; // get union of all values (which are now the keys) that are not never
 
 export type MethodNamesMatching<
-  T extends { [K in keyof T]: any },
+  T extends { [K in keyof T]: unknown },
   P extends Function = Function
 > = {
   [K in keyof T]: T[K] extends Function // is this key a function?
