adba3eca415d6a19449a45147c0d7b90a15b3a78
Seth Battis <seth@battis.net>
2026-07-01 11:38:46 -0400

    docs: document Configuration options

--- packages/pkg-setup/src/Setup/Configuration.ts
+++ packages/pkg-setup/src/Setup/Configuration.ts
@@ -1,6 +1,7 @@
 import { PathString } from '@battis/descriptive-types';
 import * as Plugin from '@qui-cli/plugin';
 
+/** Optionally returns a warning to be displayed at the end of the set up process */
 export type FileHandler = (
   srcPath: PathString,
   destPath: PathString,
@@ -8,8 +9,17 @@ export type FileHandler = (
 ) => (undefined | string) | Promise<undefined | string>;
 
 export type Configuration = Plugin.Configuration & {
+  /** Name of the package to set up */
   packageName?: string;
+  /** Directory of `packageName` to search for configuration templates */
   setupDir?: PathString;
+  /**
+   * Define custom file handling behavior (`package.json` and
+   * `pnpm-workspace.yaml` are merged together rather than replaced by default)
+   */
   fileHandlers?: Record<string, FileHandler>;
+  /** File names or patterns to ignore in `setupDir` */
+  ignore?: (string | RegExp)[];
+  /** Run without confirmation, accepting proposed changes automatically */
   force?: boolean;
 };
