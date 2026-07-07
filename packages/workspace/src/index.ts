951a8d03272394b7e761a6ed99a5aa899d9426c9
Seth Battis <seth@battis.net>
2026-07-06 23:24:57 -0400

    refactor: use Placeholders

--- packages/workspace/src/index.ts
+++ packages/workspace/src/index.ts
@@ -1,4 +1,4 @@
-import { Init } from '@qui-cli/init';
+import { Init, Placeholders } from '@qui-cli/init';
 import { Core, Positionals } from '@qui-cli/core';
 import fs from 'node:fs';
 import path from 'node:path';
@@ -11,46 +11,57 @@ const versionrc: Init.FileHandlers.FileHandler = async ({
   destPath,
   force
 }) => {
-  const pkg = JSON.parse(
-    fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8')
-  );
-  if (pkg.name) {
-    if (fs.existsSync(srcPath)) {
-      const src = fs
-        .readFileSync(srcPath, 'utf8')
-        .replaceAll(
-          '{{PATH}}',
-          path
-            .relative(path.join(appRootPath.toString()), process.cwd())
-            .replace(/^[^/]+\//, '')
-        )
-        .replaceAll('{{SCOPE}}', path.basename(process.cwd()))
-        .replaceAll('{{NAME}}', pkg.name);
-
-      await Init.Confirm.withDiff({
-        src,
-        dest: fs.existsSync(destPath)
-          ? fs.readFileSync(destPath, 'utf8')
-          : undefined,
-        identifier: Colors.path(destPath, Colors.keyword),
-        action: () => fs.writeFileSync(destPath, src),
-        force
+  const packagePath = path.join(process.cwd(), 'package.json');
+  if (fs.existsSync(packagePath)) {
+    const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+    if (pkg.name) {
+      Placeholders.configure({
+        placeholders: {
+          path: [
+            path
+              .relative(path.join(appRootPath.toString()), process.cwd())
+              .replace(/^[^/]+\//, '')
+          ],
+          scope: [path.basename(process.cwd())],
+          name: [pkg.name]
+        }
       });
+      if (fs.existsSync(srcPath)) {
+        const src = Placeholders.replaceAll(fs.readFileSync(srcPath, 'utf8'));
+
+        await Init.Confirm.withDiff({
+          src,
+          dest: fs.existsSync(destPath)
+            ? fs.readFileSync(destPath, 'utf8')
+            : undefined,
+          identifier: Colors.path(destPath, Colors.keyword),
+          action: () => fs.writeFileSync(destPath, src),
+          force
+        });
+      } else {
+        Log.error(
+          Colors.error(
+            `Could not read from ${Colors.path(srcPath, Colors.keyword)}`
+          )
+        );
+      }
     } else {
-      Log.error(
-        Colors.error(
-          `Could not read from ${Colors.path(srcPath, Colors.keyword)}`
-        )
-      );
+      return `${Colors.path(
+        destPath,
+        Colors.keyword
+      )} could not be verified because ${Colors.path(
+        path.join(process.cwd(), 'package.json'),
+        Colors.keyword
+      )} has no ${Colors.value('name')} defined.`;
     }
   } else {
-    return `${Colors.path(
-      destPath,
-      Colors.keyword
-    )} could not be verified because ${Colors.path(
-      path.join(process.cwd(), 'package.json'),
-      Colors.keyword
-    )} has no ${Colors.value('name')} defined.`;
+    return (
+      `${Colors.path(
+        destPath,
+        Colors.keyword
+      )} could not be verified because no ${Colors.path('package.json')} ` +
+      `file is present in ${Colors.path(process.cwd())}`
+    );
   }
 };
 
